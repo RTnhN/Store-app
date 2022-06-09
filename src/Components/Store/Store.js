297f292b565b4542ebd513b29e56f9eae7a5e351
Zach Strout <zachstrout@gmail.com>
2022-06-09 15:07:19 -0500

    Convert Store to functional
    There was not any fancy things that Store needed to do, so I converted
    it to a functional component.

--- src/Components/Store/Store.js
+++ src/Components/Store/Store.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../../Styles/Store.css';
 import Header from '../Sub-Components/Header/Header';
 import Footer from '../Sub-Components/Footer/Footer';
 import NamePriceCard  from '../Sub-Components/NamePriceCard/NamePriceCard';
 
-class Store extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
+function Store(props){
     return(
       <div id='store'>
-         <Header />
+         <Header itemsInCart={props.itemsInCart} />
           <div id='storeBody'>
             <h1 id='storeTitle'>Store</h1>
             <div id='storeProducts'>
-              {this.props.data.map((product) => { return (<NamePriceCard key={product.id} id={product.id} name={product.name} price={product.price} img={product.img[0]} imgAlt={product.name} /> )})}
+              {props.data.map((product) => { return (<NamePriceCard key={product.id} id={product.id} name={product.name} price={product.price} img={product.img[0]} imgAlt={product.name} /> )})}
             </div>
           </div>
          <Footer />
       </div>
-    )}
+    )
 }
             
 export default Store;
